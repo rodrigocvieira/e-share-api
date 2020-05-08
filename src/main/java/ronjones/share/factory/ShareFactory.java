8fde98c97191504a6baba6049ac2e7bfcb254761
rodrigo <rodrigo_c91@hotmail.com>
2020-05-08 17:00:24 -0300

    refactor class ShareFactory

--- src/main/java/ronjones/share/factory/ShareFactory.java
+++ src/main/java/ronjones/share/factory/ShareFactory.java
@@ -2,44 +2,30 @@ package ronjones.share.factory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ronjones.share.core.decorator.ShareDecorator;
 import ronjones.share.core.share.enuns.SocialNetwork;
 import ronjones.share.core.share.exceptions.SocialNetworkException;
-import ronjones.share.socialnetwork.facebook.adapter.FacebookResponseToShareResponse;
-import ronjones.share.socialnetwork.facebook.adapter.ShareRequestToFacebookRequestAdapter;
-import ronjones.share.socialnetwork.facebook.api.FacebookApi;
-import ronjones.share.socialnetwork.instagram.adapter.InstagramResponseToShareResponse;
-import ronjones.share.socialnetwork.instagram.adapter.ShareRequestToInstagramRequestAdapter;
-import ronjones.share.socialnetwork.instagram.api.InstagramApi;
+import ronjones.share.core.share.interfaces.ShareSocialNetworkSend;
+import ronjones.share.socialnetwork.facebook.Facebook;
+import ronjones.share.socialnetwork.instagram.Instagram;
 
 @Component
 public class ShareFactory {
 
-    private final FacebookApi facebookApi;
-    private final InstagramApi instagramApi;
+    private final Facebook facebook;
+    private final Instagram instagram;
 
     @Autowired
-    public ShareFactory(FacebookApi facebookApi,
-                        InstagramApi instagramApi) {
-        this.facebookApi = facebookApi;
-        this.instagramApi = instagramApi;
+    public ShareFactory(Facebook facebook, Instagram instagram) {
+        this.facebook = facebook;
+        this.instagram = instagram;
     }
 
-    public ShareDecorator<?, ?> build(SocialNetwork socialNetwork) {
-
+    public ShareSocialNetworkSend build(SocialNetwork socialNetwork) {
         switch (socialNetwork) {
             case FACEBOOK:
-                return new ShareDecorator<>(
-                        new ShareRequestToFacebookRequestAdapter(),
-                        new FacebookResponseToShareResponse(),
-                        facebookApi
-                );
+                return facebook;
             case INSTAGRAM:
-                return new ShareDecorator<>(
-                        new ShareRequestToInstagramRequestAdapter(),
-                        new InstagramResponseToShareResponse(),
-                        instagramApi
-                );
+                return instagram;
             default:
                 throw new SocialNetworkException(String.format("%s não implementado", socialNetwork));
         }
